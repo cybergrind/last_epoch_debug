340358d63cc7b0b57f8fe59dae9ba9a14e96d39a
Kirill Pinchuk <cybergrind@gmail.com>
2025-04-27 10:19:49 +0300

    scripts: fixed check_sums.sh

--- scripts/check_sums.sh
+++ scripts/check_sums.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-md5sum ${GAME_FOLDER}/GameAssembly.dll Metadata/global-metadata.dat > /tmp/sums && diff sums /tmp/sums
+SUMS=${PROJECT_ROOT}/notes/sums
+TMP_SUMS=/tmp/sums
+GAME_ASSEMBLY="${GAME_FOLDER}/GameAssembly.dll"
+GLOBAL_METADATA="${METADATA}/global-metadata.dat"
+
+[[ ! -f $GAME_ASSEMBLY ]] && echo "GameAssembly.dll not found at ${GAME_ASSEMBLY}" && exit 1
+[[ ! -f $GLOBAL_METADATA ]] && echo "global-metadata.dat not found at ${GLOBAL_METADATA}" && exit 1
+
+md5sum "$GAME_ASSEMBLY" "$GLOBAL_METADATA" > "$TMP_SUMS"
+# remove prefix $GAME_FOLDER
+sed -i "s|$GAME_FOLDER/||g" "$TMP_SUMS"
+
+diff -s "$SUMS" "$TMP_SUMS"
