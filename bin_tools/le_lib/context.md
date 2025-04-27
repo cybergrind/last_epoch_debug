09fa4fa26a6b9a1f2dd9577ee95d52e52fad4a89
Kirill Pinchuk <cybergrind@gmail.com>
2025-04-27 13:42:45 +0300

    le_lib: move le_lib_echo to module

--- bin_tools/le_lib/context.md
+++ bin_tools/le_lib/context.md
@@ -1,7 +1,55 @@
+This is a rust library that would be called from game code modified by loading it with `LD_PRELOAD`.
+It is used for games that runs on linux with proton or wine.
 
-This is a rust library that would be called from code.
+Rust code style:
+
+* use new module style (do not use `mod.rs` file in the nested directory, just create file in the root of the module)
+* use `#[unsafe(no_mangle)]` where required
+
+There are following functions:
+
+* `le_lib_init` - initializes the library:
+  * allocates memory for global variables
+  * sets up hook that will be called after dll is loaded in memory
+* `le_lib_echo` - dumps all registers when called from C code
+* `le_lib_load_hook` - loads the hook into the game
+* `le_lib_unload_hook` - unloads the hook from the game
+
+
+Hooks are defined in yaml file with following structure:
+```yaml
+hooks:
+  - name: hook_name
+    target_address: 0x12345678
+    # whe check memory content before modifying it
+    memory_content: '\x95\xec\x00\xc0\x00\x00\x00\x00\x17+\x01\xc0\x00\x00\x00\x00'
+    hook_function: target_function_name
+```
 
 TODO:
-* Add logging to file: /tmp/le_lib.log
-* Add simple function `le_lib_echo` that will dump all registers when called
-* Add example C code that will be compiled and injected into the game. It should save all registers and call `le_lib_echo()`
+* move `le_lib_echo` into separate file
+* add module `le_lib_init`:
+  - initialized logger
+  - setup hook that will log after dll is loaded in memory with dll name
+  - initialize global hashmap for hooks:
+    - key: hook name
+    - value: hook function address
+
+* add module for hooks functions:
+  - `le_lib_load_hook` - loads the hook into the game
+  - `le_lib_unload_hook` - unloads the hook from the game
+* `le_lib_load_hook` details:
+  - read yaml file with hooks
+  - for each hook:
+    - check if hook is already loaded
+    - if not, load it and log
+    - get address for target function
+    - complile two small assembly functions:
+      - first will:
+        - save registers information
+        - call hook_function
+        - restore registers
+        - long jump to target function after the second function call
+      - second will:
+        - long jump to first function address
+      - use following function to compile: `nasm -o /tmp/<somefile> -f elf64 -l -g -w+all`
---
+++ bin_tools/le_lib/src/echo.rs
@@ -0,0 +1,117 @@
+use log::info;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct Registers {
+    pub rax: u64,
+    pub rbx: u64,
+    pub rcx: u64,
+    pub rdx: u64,
+    pub rsi: u64,
+    pub rdi: u64,
+    pub rbp: u64,
+    pub rsp: u64,
+    pub r8: u64,
+    pub r9: u64,
+    pub r10: u64,
+    pub r11: u64,
+    pub r12: u64,
+    pub r13: u64,
+    pub r14: u64,
+    pub r15: u64,
+    pub rip: u64,
+    pub rflags: u64,
+}
+
+/// Captures current register state with no-op assembly that preserves all registers
+unsafe fn capture_registers() -> Registers {
+    let mut regs = Registers {
+        rax: 0, rbx: 0, rcx: 0, rdx: 0,
+        rsi: 0, rdi: 0, rbp: 0, rsp: 0,
+        r8: 0, r9: 0, r10: 0, r11: 0,
+        r12: 0, r13: 0, r14: 0, r15: 0,
+        rip: 0, rflags: 0,
+    };
+
+    #[cfg(target_arch = "x86_64")]
+    unsafe {
+        // First block: capture first set of registers
+        std::arch::asm!(
+            "mov {rax}, rax",
+            "mov {rbx}, rbx",
+            "mov {rcx}, rcx",
+            "mov {rdx}, rdx",
+            "mov {rsi}, rsi",
+            "mov {rdi}, rdi",
+            "mov {rbp}, rbp",
+            "mov {rsp}, rsp",
+            "pushfq",
+            "pop {rflags}",
+            rax = out(reg) regs.rax,
+            rbx = out(reg) regs.rbx,
+            rcx = out(reg) regs.rcx,
+            rdx = out(reg) regs.rdx,
+            rsi = out(reg) regs.rsi,
+            rdi = out(reg) regs.rdi,
+            rbp = out(reg) regs.rbp,
+            rsp = out(reg) regs.rsp,
+            rflags = out(reg) regs.rflags,
+        );
+
+        // Second block: capture remaining registers
+        std::arch::asm!(
+            "mov {r8}, r8",
+            "mov {r9}, r9",
+            "mov {r10}, r10",
+            "mov {r11}, r11",
+            "mov {r12}, r12",
+            "mov {r13}, r13",
+            "mov {r14}, r14",
+            "mov {r15}, r15",
+            r8 = out(reg) regs.r8,
+            r9 = out(reg) regs.r9,
+            r10 = out(reg) regs.r10,
+            r11 = out(reg) regs.r11,
+            r12 = out(reg) regs.r12,
+            r13 = out(reg) regs.r13,
+            r14 = out(reg) regs.r14,
+            r15 = out(reg) regs.r15,
+        );
+
+        // Get an approximation of RIP by taking the return address from the stack
+        std::arch::asm!(
+            "lea {rip}, [rip]",
+            rip = out(reg) regs.rip,
+        );
+    }
+
+    regs
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn le_lib_echo() {
+    crate::initialize_logger();
+
+    // Safely capture registers
+    let regs = unsafe { capture_registers() };
+
+    info!("Register dump:");
+    info!("RAX: 0x{:016x}", regs.rax);
+    info!("RBX: 0x{:016x}", regs.rbx);
+    info!("RCX: 0x{:016x}", regs.rcx);
+    info!("RDX: 0x{:016x}", regs.rdx);
+    info!("RSI: 0x{:016x}", regs.rsi);
+    info!("RDI: 0x{:016x}", regs.rdi);
+    info!("RBP: 0x{:016x}", regs.rbp);
+    info!("RSP: 0x{:016x}", regs.rsp);
+    info!("R8:  0x{:016x}", regs.r8);
+    info!("R9:  0x{:016x}", regs.r9);
+    info!("R10: 0x{:016x}", regs.r10);
+    info!("R11: 0x{:016x}", regs.r11);
+    info!("R12: 0x{:016x}", regs.r12);
+    info!("R13: 0x{:016x}", regs.r13);
+    info!("R14: 0x{:016x}", regs.r14);
+    info!("R15: 0x{:016x}", regs.r15);
+    info!("RIP: 0x{:016x}", regs.rip);
+    info!("RFLAGS: 0x{:016x}", regs.rflags);
+}
