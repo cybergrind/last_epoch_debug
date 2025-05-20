e25fe3da07751937355bdf4029bdd072bd81859b
Kirill Pinchuk <cybergrind@gmail.com>
2025-05-20 15:16:29 +0300

    le_lib: tune cast speedup parameters

--- bin_tools/le_lib/src/hooks/ability_hook.rs
+++ bin_tools/le_lib/src/hooks/ability_hook.rs
@@ -74,10 +74,10 @@ impl CastingData {
     pub fn set_instant_cast_from_ptr(ptr: u64) {
         unsafe {
             let inc_cast_speed_ptr = (ptr + 0x1c) as *mut f32;
-            *inc_cast_speed_ptr = 1000.0;
+            *inc_cast_speed_ptr = 100.0;
 
             let use_duration = (ptr + 0x7c) as *mut f32;
-            *use_duration = 0.03;
+            *use_duration = 0.07;
 
             // let use_delay = (ptr + 0x80) as *mut f32;
             // *use_delay = 0.001;
